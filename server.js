98f77d95fa053e53ff20ef704d659a0fc3560af9
Luis <luis@netbeast.co>
2016-02-09 12:31:59 +0100

    Fixed bug and detect if arp entry is incomplete

--- server.js
+++ server.js
@@ -16,17 +16,16 @@ function getArp () {
 	})
 }
 
-
 function parse_arp_table(arpt) {
 
 	var arp_arr = []
-	var arp_obj = {}
 
 	//Split array
 	arpt = arpt.split('\n'); 
 
 	var aux
 	for (aux in arpt) {
+		var arp_obj = {}
 		var entry = arpt[aux];
 
 
@@ -37,27 +36,23 @@ function parse_arp_table(arpt) {
         //Get the IP
         var ip = entry.slice(ip_start, ip_end);
  
-        if(ip) {
-        	arp_obj['ip']= ip
-        }
+        if(ip) arp_obj['ip']= ip
 
         //Get the interface
         var interface_start = entry.indexOf('on') + 3
         var interface_end = entry[entry.length]
         var interface = entry.slice(interface_start, interface_end)
         
-        if(interface) {
-        	arp_obj['interface'] = interface
-        	arp_arr.push(arp_obj)
-        }
+        if(interface) arp_obj['interface'] = interface
 
         //Get the position where MAC starts
         var mac_start = entry.indexOf(':') -2
-        var mac_end = mac_start + 17;
+        var mac_end = mac_start + 17
         var mac = entry.slice(mac_start, mac_end)
 
         if(mac) {
         	arp_obj['mac'] = mac
+        	arp_arr.push(arp_obj)
         }
     }
     return arp_arr;
